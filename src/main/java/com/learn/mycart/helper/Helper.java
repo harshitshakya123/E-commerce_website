9c7b7463d3d6cd1681e9a69ee38e04345b3e2b29
Harshit Shakya <43888267+123harshitshakya@users.noreply.github.com>
2020-08-12 14:41:39 +0530

    Update Helper.java

--- src/main/java/com/learn/mycart/helper/Helper.java
+++ src/main/java/com/learn/mycart/helper/Helper.java
@@ -1,5 +1,12 @@
 package com.learn.mycart.helper;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
 public class Helper {
 
 		 
@@ -21,5 +28,28 @@ public class Helper {
 		{
 			return (desc+"...");
 		}
+	}
+	
+	public static Map<String,Long> getCounts(SessionFactory factory)
+	{
+		Session session = factory.openSession();
+		String q1="Select count(*) from User";
+		String q2="Select count(*) from Product";
+		
+		Query query1 = session.createQuery(q1);
+		Query query2 = session.createQuery(q2);
+		
+		Long userCount = (Long) query1.list().get(0);
+		Long productCount = (Long)query2.list().get(0);
+		
+		Map<String, Long> map=new HashMap<String, Long>();
+		map.put("userCount", userCount);
+		map.put("productCount", productCount);
+		
+		
+		
+		session.close();
+		
+		return map;
 	}
 }
